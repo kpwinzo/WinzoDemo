9bc3c1be9f54c9e7115fecea9c6948c2e7905343
kush1833 <kush97pratap@gmail.com>
2021-06-02 09:22:36 +0530

    Modified demo

--- hw.cpp
+++ hw.cpp
@@ -1,5 +1,11 @@
 #include<stdio.h>
 using namespace std;
+void getName(){
+	string name;
+	cin >> name;
+	cout << "Hello "<<name<<" to the world."<< endl;
+}
 int main(){
 	cout << "Hello World"<<endl;
+	getName();
 }
